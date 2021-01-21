2b0744eb6c966aceacf3049ad673340b838a188a
Shunsuke Yamazaki <s.yamazaki@everyleaf.com>
2021-01-21 22:26:27 +0900

    404を返すように変更

--- src/main.rs
+++ src/main.rs
@@ -17,12 +17,22 @@ fn handle_connection(mut stream: TcpStream) {
   let mut buffer = [0; 1024];
   stream.read(&mut buffer).unwrap();
 
-  let mut file = File::open("hello.html").unwrap();
+  let get = b"GET / HTTP/1.1\r\n";
 
+  let (status_line, filename) = if buffer.starts_with(get) {
+    ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+  } else {
+    ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+  };
+
+  let mut file = File::open(filename).unwrap();
   let mut contens = String::new();
+
   file.read_to_string(&mut contens).unwrap();
 
-  let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contens);
+  let response = format!("{}{}", status_line, contens);
+
+  file.read_to_string(&mut contens).unwrap();
 
   stream.write(response.as_bytes()).unwrap();
   stream.flush().unwrap();
