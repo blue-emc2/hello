799ef75d796e360ce484056d678298f1e65bd371
Shunsuke Yamazaki <s.yamazaki@everyleaf.com>
2021-01-22 17:17:18 +0900

    workerを作成する

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
-pub struct ThreadPool;
+use std::thread;
+
+pub struct ThreadPool {
+  workers: Vec<Worker>,
+}
 
 impl ThreadPool {
   /// 新しいThreadPoolを生成する
@@ -8,12 +12,35 @@ impl ThreadPool {
   pub fn new(size: usize) -> ThreadPool {
     assert!(size > 0);
 
-    ThreadPool
+    let mut workers = Vec::with_capacity(size);
+
+    for id in 0..size {
+      let worker = Worker::new(id);
+      workers.push(worker);
+    }
+
+    ThreadPool { workers }
   }
 
   pub fn execute<F>(&self, f: F)
   where
     F: FnOnce() + Send + 'static,
   {
+  }
+}
+
+struct Worker {
+  id: usize,
+  handle: thread::JoinHandle<()>,
+}
+
+impl Worker {
+  fn new(id: usize) -> Worker {
+    let thread = thread::spawn(|| {});
+
+    Worker {
+      id: id,
+      handle: thread,
+    }
   }
 }
